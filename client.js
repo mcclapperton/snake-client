a7a6ab61d118bb5bff74f9e763b11662025b5208
Maris <mcclapperton@gmail.com>
2022-10-26 17:29:12 -0600

    playing around with moves to client

--- client.js
+++ client.js
@@ -4,8 +4,21 @@ const connect = function () {
     host: "165.227.47.243",
     port: "50541",
   });
-  conn.setEncoding("utf8");
   // interpret incoming data as text
+  conn.setEncoding("utf8");
+  // say when connection est
+  conn.on("connect", () => {
+    console.log(`ready to rock n roll - cxn est.`);
+    conn.write(`Name: MEC`);
+    //  ` setInterval(
+    //     (funk = () => {
+    //       conn.write("Move: up");
+    //       conn.write("Move: right");
+    //       conn.write("Move: down");
+    //     }),
+    //     500
+    //   );`
+  });
   conn.on("data", (data) => {
     console.log(data);
   });
