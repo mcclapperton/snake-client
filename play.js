4935ecafe6b03632c9152ad3ea3da6f790682f62
Maris <mcclapperton@gmail.com>
2022-10-26 16:50:07 -0600

    linked play and client files, moved connection fxn to client

---
+++ play.js
@@ -0,0 +1,7 @@
+const net = require("net");
+const { connect } = require("./client");
+// establishes a connection with the game server
+
+// const conn = connect();
+console.log("Connecting ...");
+connect();
