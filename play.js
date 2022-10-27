1ef2bac81aea9d41c5dd352a4da10232b922d69a
Maris <mcclapperton@gmail.com>
2022-10-26 18:18:14 -0600

    adding ctrl c to play.js to exit

--- play.js
+++ play.js
@@ -1,7 +1,22 @@
-const net = require("net");
 const { connect } = require("./client");
 // establishes a connection with the game server
+const handleUserInput = function (key) {
+  // \u0003 maps to ctrl+c input to exit
+  if (key === "\u0003") {
+    process.exit();
+  }
+};
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.on("data", handleUserInput);
+  stdin.resume();
 
+  return stdin;
+};
 // const conn = connect();
 console.log("Connecting ...");
 connect();
+setupInput();
